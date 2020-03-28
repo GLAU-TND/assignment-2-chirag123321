ab36805d561ef5f57d3b51ca8f4ce9a40accda1b
chirag123321 <60148102+chirag123321@users.noreply.github.com>
2020-03-28 10:27:01 +0530

    Update MyMain.java

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -1,3 +1,4 @@
+  
 /*
  *  Created by IntelliJ IDEA.
  *  User: Vaibhav
@@ -5,7 +6,21 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+import problem5.student.Student;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String args[]) {
+        MyPriorityQueue obj = new MyPriorityQueue();
+        obj.insert("Yagay",59);
+        obj.insert("Mohan",19);
+        obj.insert("Mukund",20);
+        obj.insert("Amit",2);
+        obj.insert("Umang",53);
+        System.out.println("Students according to their roll number are: ");
+        obj.peep();
+    }
 }
